8c4a54bb45b1a9efac58324e0d37dbca142be8c6
Seth <98078112+GeneralFatPug@users.noreply.github.com>
2024-09-06 20:45:46 -0500

    add global variables for levels

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -1,18 +1,19 @@
 extends Node
 
-@export var _current_level: Node3D
-
-@export var _test_level: Node3D
-@export var _cargoyard_level: Node3D
+var _current_level: Node3D
 #add more levels as they are made
 
 enum {TEST, CARGO}
 
+func _ready():
+	#for now we will just start at the cargo level
+	_current_level = CargoLevel
+
+func get_curr_level():
+	return _current_level
+
 func set_curr_level(level_selected):
 	if level_selected == TEST:
-		_current_level = _test_level 
+		_current_level = TestLevel
 	elif level_selected == CARGO:
-		_current_level = _cargoyard_level
-
-func get_curr_level_objectives_node():
-	_current_level.get_objective_node()
+		_current_level = CargoLevel

96baf032a6f07f5347a435f931e7e30af576a2bc
Seth <98078112+GeneralFatPug@users.noreply.github.com>
2024-10-30 15:35:40 -0500

    add more work for objectives

---
+++ Scripts/PickupBun.gd
@@ -0,0 +1,5 @@
+extends Area3D
+
+func _on_area_entered(area):
+	print("Bun entered ", area)
+	
---
+++ Scripts/PickupMustard.gd
@@ -0,0 +1,6 @@
+class_name PickupBase
+extends Area3D
+
+func _on_area_entered(area):
+	print("Mustard entered ", area)
+	

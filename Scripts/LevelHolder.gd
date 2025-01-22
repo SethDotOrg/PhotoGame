1a16ac3ab27c613f00a3c3181fdc8d56af75c7e3
Seth <98078112+GeneralFatPug@users.noreply.github.com>
2025-01-21 22:36:21 -0600

    start new branch to raise the style of the game

--- Scripts/LevelHolder.gd
+++ Scripts/LevelHolder.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+@export var _prototype_level: Node3D
+@export var _cargo_level: Node3D
+
+func _ready():
+	if GlobalVariables._current_level == GlobalVariables.PROTOTYPE:
+		_prototype_level.visible = true
+		_cargo_level.visible = false
+	if GlobalVariables._current_level == GlobalVariables.CARGO:
+		_prototype_level.visible = false
+		_cargo_level.visible = true
 
 func get_player_node():
 	return get_parent().get_node("PlayerHolder").get_player_node()
